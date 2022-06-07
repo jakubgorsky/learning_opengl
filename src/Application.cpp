113d55570d2f60e437f684ce8ecb9cfba73ba965
jakub_gorsky <kuba.gorsky@gmail.com>
2022-06-07 20:07:26 +0200

    Implemented ImGui!

--- src/Application.cpp
+++ src/Application.cpp
@@ -13,6 +13,10 @@
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
 
+#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+
 int main()
 {
     GLFWwindow* window;
@@ -27,7 +31,7 @@ int main()
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(1024, 1024, "Abstracting OpenGL into classes", nullptr, nullptr);
+    window = glfwCreateWindow(960, 540, "ImGui!!!!", nullptr, nullptr);
     if (!window)
     {
         glfwTerminate();
@@ -36,17 +40,18 @@ int main()
 
     /* Make the window's context current  */
     glfwMakeContextCurrent(window);
-    glfwSwapInterval(4);
+    glfwSwapInterval(1);
 
     glewInit();
 
     std::cout << glGetString(GL_VERSION) << std::endl;
     {
+        // img = 1293 x 1776
         float positions1[] = {
-                -0.5f, -0.5f, 0.0f, 0.0f,
-                0.5f, -0.5f, 1.0f, 0.0f,
-                0.5f, 0.5f, 1.0f, 1.0f,
-                -0.5f, 0.5f, 0.0f, 1.0f
+                200.0f, 200.0f, 0.0f, 0.0f,
+                329.3f, 200.0f, 1.0f, 0.0f,
+                329.3f, 377.6f, 1.0f, 1.0f,
+                200.0f, 377.6f, 0.0f, 1.0f
         };
 
         unsigned int indices[] = {
@@ -71,13 +76,16 @@ int main()
 
         IndexBuffer ib(indices, 6);
 
-        glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+        glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+        glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+        glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 100, 0));
 
+        glm::mat4 mvp = proj * view * model;
 
         Shader shader("../res/shaders/Basic.shader");
         shader.Bind();
         shader.SetUniform4f("u_Color", 0.0f, 0.0f, 0.0f, 0.0f);
-        shader.SetUniformMat4f("u_MVP", proj);
+        shader.SetUniformMat4f("u_MVP", mvp);
 
         Texture tex("../res/textures/yerbaholic.png");
         tex.Bind();
@@ -90,19 +98,36 @@ int main()
 
         Renderer renderer;
 
-        int r1, r2, g1, g2, b1, b2;
-        r1 = 100;
-        g1 = 0;
-        b1 = 0;
-        r2 = 0;
-        g2 = 100;
-        b2 = 0;
-        int increment = 5;
+
+        // IMGUI
+        const char* glsl_version = "#version 330 core";
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui::StyleColorsDark();
+        ImGui_ImplOpenGL3_Init(glsl_version);
+
+        bool show_demo_window = true;
+        bool show_another_window = false;
+        ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+//        int r1, r2, g1, g2, b1, b2;
+//        r1 = 100;
+//        g1 = 0;
+//        b1 = 0;
+//        r2 = 0;
+//        g2 = 100;
+//        b2 = 0;
+//        int increment = 5;
 
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window)) {
             /* Render here */
             renderer.Clear();
+
+            ImGui_ImplOpenGL3_NewFrame();
+            ImGui_ImplGlfw_NewFrame();
+            ImGui::NewFrame();
 
             shader.Bind();
 //            shader.SetUniform4f("u_Color", (float) r1 / 100, (float) g1 / 100, (float) b1 / 100, 0.33f);
@@ -110,7 +135,7 @@ int main()
             GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
             shader.Unbind();
 
-//            // rainbow effect
+            // rainbow effect
 //            if (r1 == 100 && g1 < 100 && b1 == 0) {
 //                g1 += increment;
 //            } else if (r1 > 0 && g1 == 100 && b1 == 0) {
@@ -125,6 +150,33 @@ int main()
 //                b1 -= increment;
 //            }
 
+            if (show_demo_window)
+                ImGui::ShowDemoWindow(&show_demo_window);
+            {
+                static float f = 0.0f;
+                static int counter = 0;
+
+                ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+                ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+                ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+                ImGui::Checkbox("Another Window", &show_another_window);
+
+                ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+                ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+                if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+                    counter++;
+                ImGui::SameLine();
+                ImGui::Text("counter = %d", counter);
+
+                ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+                ImGui::End();
+            }
+
+            ImGui::Render();
+            ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
 
@@ -132,6 +184,10 @@ int main()
             glfwPollEvents();
         }
     }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }
